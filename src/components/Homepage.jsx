356971ed7ea2630cd9f814ded788ca7c56e87b2d
Ranjit Kalita <ranjitkalita499@gmail.com>
2023-11-02 23:54:14 +0530

    changes

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,7 +5,7 @@ import Section3 from '../sections/Section3';
 import Section4 from '../sections/Section4';
 import Section5 from '../sections/Section5';
 import Section6 from '../sections/Section6';
-import Section7 from '../sections/Section7';
+// import Section7 from '../sections/Section7';
 import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
 const Homepage = () => {
@@ -28,7 +28,7 @@ const Homepage = () => {
       <Section4 />
       <Section5 />
       <Section6 />
-      <Section7 />
+      {/* <Section7 /> */}
     </div>
   );
 };
